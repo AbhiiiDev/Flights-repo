879a545febecc08b24258d63cae2129346322a3a
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2024-02-20 21:01:11 +0530

    completed migration for flight creation and also implemented foreign key

--- src/index.js
+++ src/index.js
@@ -11,19 +11,7 @@ app.use(express.urlencoded({extended:true}));
 app.use('/api',apiRoutes);
 
 
-app.listen(ServerConfig.PORT,async ()=>{
+app.listen(ServerConfig.PORT, ()=>{
     console.log(` successfully started server on ${ServerConfig.PORT}`)
     LoggerConfig.info("successfully started the server")
-
-    const {Airport,City} =require('./models');
-   const city=await City.findByPk(3);
-// console.log(city);
-
-// const airport=await city.createAirport({name:'Safdarjung Airport, Satya Sadan',address:'Sri Aurobindo Marg, near Jorbagh, Satya Sadan, New Delhi, Delhi 110003', code:'VIDD'});
-
-//     console.log(airport);
-
-    const airport=await city.getAirports();
-    console.log(airport)
-
 })
